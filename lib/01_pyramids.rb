5e4cb0b83202fafee38a3b813d675a1de3c39e58
LouisKzy <louiskrejezy@gmail.com>
2023-10-05 00:10:29 +0200

    final avec erreur

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -8,6 +8,18 @@ def welcome
   return floor_number
 end
 
+def half_pyramid(floor_number)
+  actual_floor = 1
+  while actual_floor <= floor_number
+    space = " " * (floor_number - actual_floor) # gèrerles espaces au début 
+    carct = "#" * actual_floor # gère res "#" affiché
+    puts space + carct  # affiche la pyramide à "l'envers"
+    actual_floor += 1 # rncrémente les étage de 1
+  end
+  return floor_number
+end
+
+
 # afficher une première moitié haute de pyramide
 def pyramid_up(floor_number)
   etage = welcome 
@@ -22,8 +34,8 @@ def pyramid_up(floor_number)
 end
 
 # affiché une deuxième moitié mais partant de la base de la pyramide d d'avant ( petit génie)
-def pyramid_down(floor_number, start2)
-  nb_spaces = 1
+def pyramid_down(floor_number)
+  nb_spaces = 2
   until floor_number <= 0  #tant que 0 < 10  boucle
     spaces_d = " " * nb_spaces    # 1(" " * 0 )== ""  // 2(" " * 1) == " " // 3(" "*2)== "  "
     hashtag = "#" *  (floor_number - 1)   #  1("#"*10/2)==(5+1) == 6 // 2("#" * 9/2)
@@ -33,17 +45,20 @@ def pyramid_down(floor_number, start2)
   end
 end
 
-def full_pyramid
+def wtf_pyramid
   call1 = welcome 
-  call2 = pyr_up(call)
-  pyramid_down(call1, call2)
+  call2 = pyramid_up(call1)
+  pyramid_down(call2)
 end
 
 def perform
-  full_pyramid()
+  call1 = welcome 
+  half_pyramid(call1)
+  pyramid_up(call1)
+  wtf_pyramid()
 end
 
-call1 = welcome 
-pyramide_up(call1)
+
+
 #s'arracher les cheveux a ce que cela marche. (dédicace aux chauves)
 perform()
